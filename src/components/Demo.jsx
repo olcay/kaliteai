4e264f0374430f9b2a7f3c724613ab769e29aff8
Olcay Bayram <olcay@users.noreply.github.com>
2023-09-04 21:32:21 +0200

    Demo component re-design

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,56 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-import { copy, linkIcon, loader, tick } from "../assets";
+import { linkIcon, loader } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
+
+import styles, { layout } from "../style";
 
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
     summary: "",
   });
-  const [allArticles, setAllArticles] = useState([]);
-  const [copied, setCopied] = useState("");
 
-  // RTK lazy query
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
-
-  // Load data from localStorage on mount
-  useEffect(() => {
-    const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem("articles")
-    );
-
-    if (articlesFromLocalStorage) {
-      setAllArticles(articlesFromLocalStorage);
-    }
-  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const existingArticle = allArticles.find(
-      (item) => item.url === article.url
-    );
-
-    if (existingArticle) return setArticle(existingArticle);
-
     const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
-      const updatedAllArticles = [newArticle, ...allArticles];
 
-      // update state and local storage
       setArticle(newArticle);
-      setAllArticles(updatedAllArticles);
-      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
-  };
-
-  // copy the url and toggle the icon for user feedback
-  const handleCopy = (copyUrl) => {
-    setCopied(copyUrl);
-    navigator.clipboard.writeText(copyUrl);
-    setTimeout(() => setCopied(false), 3000);
   };
 
   const handleKeyDown = (e) => {
@@ -60,86 +31,104 @@ const Demo = () => {
   };
 
   return (
-    <section className='mt-16 w-full max-w-xl'>
-      {/* Search */}
-      <div className='flex flex-col w-full gap-2'>
-        <form
-          className='relative flex justify-center items-center'
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Let’s try our service!
+        </h2>
+        <form className='w-full  h-full'
+
           onSubmit={handleSubmit}
         >
-          <img
-            src={linkIcon}
-            alt='link-icon'
-            className='absolute left-0 my-2 ml-3 w-5'
-          />
+          <div className='relative flex justify-center items-start w-full h-full'>
+            <img
+              src={linkIcon}
+              alt='link-icon'
+              className='absolute left-0 my-2 ml-3 w-5'
+            />
 
-          <input
-            type='url'
-            placeholder='Paste the article link'
-            value={article.url}
-            onChange={(e) => setArticle({ ...article, url: e.target.value })}
-            onKeyDown={handleKeyDown}
-            required
-            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
-          />
-          <button
-            type='submit'
-            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
-          >
-            <p>↵</p>
-          </button>
+            <input
+              type='url'
+              placeholder='Paste the website link'
+              value={article.url}
+              onChange={(e) => setArticle({ ...article, url: e.target.value })}
+              onKeyDown={handleKeyDown}
+              required
+              className='url_input peer/url' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+            />
+            <button
+              type='submit'
+              className='submit_btn peer-focus/url:border-gray-700 peer-focus/url:text-gray-700 '
+            >
+              <p>↵</p>
+            </button>
+          </div>
+          <fieldset className={`${styles.paragraph} pt-5`}>
+
+            <input id="eCommerce" className="mr-2 peer/eCommerce" type="radio" name="status" defaultChecked />
+            <label htmlFor="eCommerce" className="pr-10 peer-checked/eCommerce:text-sky-500 cursor-pointer">E-commerce</label>
+
+            <input id="saas" className="mr-2 peer/saas" type="radio" name="status" />
+            <label htmlFor="saas" className="pr-10 peer-checked/saas:text-sky-500 cursor-pointer">SaaS</label>
+
+            <input id="blog" className="mr-2 peer/blog" type="radio" name="status" />
+            <label htmlFor="blog" className="peer-checked/blog:text-sky-500 cursor-pointer">Blog</label>
+
+            <div className="hidden peer-checked/eCommerce:block">I want you to buy a few items and check if you have same amount of items in the shopping cart.</div>
+            <div className="hidden peer-checked/saas:block">I want you to find the login page for users and check if the login form has proper validations.</div>
+            <div className="hidden peer-checked/blog:block">I want you to visit a blog post and check if the title is same as the link you clicked.</div>
+          </fieldset>
         </form>
 
-        {/* Browse History */}
-        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
-          {allArticles.reverse().map((item, index) => (
-            <div
-              key={`link-${index}`}
-              onClick={() => setArticle(item)}
-              className='link_card'
-            >
-              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
-                <img
-                  src={copied === item.url ? tick : copy}
-                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
-                  className='w-[40%] h-[40%] object-contain'
-                />
-              </div>
-              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
-                {item.url}
-              </p>
-            </div>
-          ))}
-        </div>
+
       </div>
 
-      {/* Display Result */}
-      <div className='my-10 max-w-full flex justify-center items-center'>
+      <div className={`${layout.sectionImg} flex-col`}>
         {isFetching ? (
           <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
         ) : error ? (
-          <p className='font-inter font-bold text-black text-center'>
+          <p className='font-bold text-black text-center'>
             Well, that wasn't supposed to happen...
             <br />
-            <span className='font-satoshi font-normal text-gray-700'>
+            <span className='font-normal text-gray-700'>
               {error?.data?.error}
             </span>
           </p>
         ) : (
           article.summary && (
             <div className='flex flex-col gap-3'>
-              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+              <h2 className=' font-bold text-gray-600 text-xl'>
                 Article <span className='blue_gradient'>Summary</span>
               </h2>
               <div className='summary_box'>
-                <p className='font-inter font-medium text-sm text-gray-700'>
+                <p className=' font-medium text-sm text-gray-700'>
                   {article.summary}
                 </p>
               </div>
             </div>
           )
         )}
+
+        <div className='flex flex-col gap-3'>
+
+          <div className='summary_box'>
+            <h2 className='font-bold text-xl border-b-white border-b-2'>
+              <div className="flex items-center h-8 space-x-1.5 px-3">
+                <div className="w-2.5 h-2.5 bg-slate-600 rounded-full"></div>
+                <div className="w-2.5 h-2.5 bg-slate-600 rounded-full"></div>
+                <div className="w-2.5 h-2.5 bg-slate-600 rounded-full"></div>
+              </div>
+            </h2>
+            <p className={`${styles.paragraph} pt-5 p-4`}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
       </div>
+
+
+
+
     </section>
   );
 };
